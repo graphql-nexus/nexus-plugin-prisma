27dae9b8c01fe459934cc73b7f7718ce5e3e20ae
schickling <schickling.j@gmail.com>
2018-12-08 20:22:45 +0100

    updated types

--- example/src/index.ts
+++ example/src/index.ts
@@ -1,13 +1,10 @@
 import { GraphQLServer } from 'graphql-yoga'
-import { Context } from './types'
 import { prisma } from './generated/prisma-client'
 import { schema } from './schema'
 
 const server = new GraphQLServer({
   schema,
-  context: {
-    prisma,
-  } as Context,
+  context: { prisma },
 })
 
 server.start(() => console.log(`🚀 Server ready at http://localhost:4000`))
