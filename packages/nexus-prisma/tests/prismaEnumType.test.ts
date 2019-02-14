a4a7427429d98f2d5a834da8f2c061d639c4a96f
Flavian DESVERNE <flavian.desverne@epitech.eu>
2019-02-14 18:24:12 +0100

    Add simple test for prismaEnumType

---
+++ packages/nexus-prisma/tests/prismaEnumType.test.ts
@@ -0,0 +1,19 @@
+import { GraphQLEnumType } from 'graphql'
+import { prismaEnumType } from '../src'
+import { mockSchema } from './prisma/mockSchema'
+
+describe('prismaEnumType', () => {
+  it('exposes only id_ASC and id_DESC', () => {
+    const PostOrderByInput = prismaEnumType({
+      name: 'PostOrderByInput',
+      members: ['id_ASC', 'id_DESC'],
+    })
+
+    const schema = mockSchema([PostOrderByInput])
+
+    const enumType = schema.getType('PostOrderByInput') as GraphQLEnumType
+    const enumValues = enumType.getValues().map(member => member.name)
+
+    expect(enumValues).toEqual(expect.arrayContaining(['id_ASC', 'id_DESC']))
+  })
+})
