71a300c5e20150ce59d73508f37796d1765b5dda
Flavian DESVERNE <flavian.desverne@epitech.eu>
2019-03-08 10:55:56 +0100

    Add validation for embeddedTypes

--- packages/nexus-prisma/src/index.ts
+++ packages/nexus-prisma/src/index.ts
@@ -2,53 +2,12 @@ import { GraphQLSchema } from 'graphql'
 import { core } from 'nexus'
 import { PrismaSchemaBuilder } from './builder'
 import { PrismaSchemaConfig } from './types'
+import { validateOptions } from './validate'
 
 export * from './definitions'
 
 interface PrismaSchemaConfigRequiredTypes extends PrismaSchemaConfig {
   types: any
-}
-
-function validateOptions(options: PrismaSchemaConfig): void {
-  if (!options.prisma) {
-    throw new Error(
-      'Missing `prisma` property in `makePrismaSchema({ prisma: { ... } })`',
-    )
-  }
-
-  if (!options.prisma.datamodelInfo) {
-    throw new Error(
-      'Missing `prisma.datamodelInfo` property in `makePrismaSchema({ prisma: { datamodelInfo: ... } })`',
-    )
-  }
-
-  if (
-    !options.prisma.datamodelInfo.uniqueFieldsByModel ||
-    !options.prisma.datamodelInfo.clientPath ||
-    !options.prisma.datamodelInfo.schema
-  ) {
-    throw new Error(
-      'Invalid `prisma.datamodelInfo` property. This should be imported from the `nexus-prisma-generate` output directory',
-    )
-  }
-
-  if (!options.prisma.client) {
-    throw new Error(
-      'Missing `prisma.client` property in `makePrismaSchema({ prisma: { client: ... } })`',
-    )
-  }
-
-  if (
-    typeof options.prisma.client !== 'function' &&
-    (!options.prisma.client.$exists || !options.prisma.client.$graphql)
-  ) {
-    throw new Error(
-      `\
-Invalid \`prisma.client\` property in \`makePrismaSchema({ prisma: { client: ... } })\`.
-This should either be an instance of the generated prisma-client, or a function that returns the prisma-client instance from your GraphQL server context
-`,
-    )
-  }
 }
 
 export function makePrismaSchema(options: PrismaSchemaConfig): GraphQLSchema {
