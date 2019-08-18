2728ea1804271f1c5d5a224f17f5fc080c68b2f6
Jason Kuhrt <jasonkuhrt@me.com>
2019-08-18 13:54:59 -0400

    jest watch typeahead plugin

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  watchPlugins: [
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname'
+  ],
   globals: {
     'ts-jest': {
       diagnostics: {
