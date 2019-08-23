2e885be048c5ebd1b320bf4fd9c5e498b1510229
Jason Kuhrt <jasonkuhrt@me.com>
2019-08-22 21:35:35 -0400

    chore: un-nest tests

--- tests/schema.test.ts
+++ tests/schema.test.ts
@@ -4,29 +4,28 @@ import { generateSchema } from './__utils'
 
 jest.setTimeout(20000)
 
-describe('schema generation', () => {
-  test('simple schema', async () => {
-    const User = objectType({
-      name: 'User',
-      definition(t: any) {
-        t.model.id()
-        t.model.name()
-      },
-    })
-    const datamodel = `
+test('simple schema', async () => {
+  const User = objectType({
+    name: 'User',
+    definition(t: any) {
+      t.model.id()
+      t.model.name()
+    },
+  })
+  const datamodel = `
     model User {
       id    Int @id
       name  String
     }
     `
 
-    const schema = await generateSchema(datamodel, [User])
+  const schema = await generateSchema(datamodel, [User])
 
-    expect(printSchema(schema)).toMatchSnapshot()
-  })
+  expect(printSchema(schema)).toMatchSnapshot()
+})
 
-  test('it exposes only pagination on relations by default', async () => {
-    const datamodel = `
+test('it exposes only pagination on relations by default', async () => {
+  const datamodel = `
     model User {
       id    Int @id
       posts Post[]
@@ -36,27 +35,27 @@ describe('schema generation', () => {
       id    Int @id
     }
     `
-    const User = objectType({
-      name: 'User',
-      definition(t: any) {
-        t.model.id()
-        t.model.posts()
-      },
-    })
-    const Post = objectType({
-      name: 'Post',
-      definition(t: any) {
-        t.model.id()
-      },
-    })
-
-    const schema = await generateSchema(datamodel, [User, Post])
-
-    expect(printSchema(schema)).toMatchSnapshot()
+  const User = objectType({
+    name: 'User',
+    definition(t: any) {
+      t.model.id()
+      t.model.posts()
+    },
+  })
+  const Post = objectType({
+    name: 'Post',
+    definition(t: any) {
+      t.model.id()
+    },
   })
 
-  test('it exposes filtering only if filtering: true', async () => {
-    const datamodel = `
+  const schema = await generateSchema(datamodel, [User, Post])
+
+  expect(printSchema(schema)).toMatchSnapshot()
+})
+
+test('it exposes filtering only if filtering: true', async () => {
+  const datamodel = `
     model User {
       id    Int @id
       posts Post[]
@@ -66,27 +65,27 @@ describe('schema generation', () => {
       id    Int @id
     }
     `
-    const User = objectType({
-      name: 'User',
-      definition(t: any) {
-        t.model.id()
-        t.model.posts({ filtering: true })
-      },
-    })
-    const Post = objectType({
-      name: 'Post',
-      definition(t: any) {
-        t.model.id()
-      },
-    })
-
-    const schema = await generateSchema(datamodel, [User, Post])
-
-    expect(printSchema(schema)).toMatchSnapshot()
+  const User = objectType({
+    name: 'User',
+    definition(t: any) {
+      t.model.id()
+      t.model.posts({ filtering: true })
+    },
+  })
+  const Post = objectType({
+    name: 'Post',
+    definition(t: any) {
+      t.model.id()
+    },
   })
 
-  test('it exposes only id filters', async () => {
-    const datamodel = `
+  const schema = await generateSchema(datamodel, [User, Post])
+
+  expect(printSchema(schema)).toMatchSnapshot()
+})
+
+test('it exposes only id filters', async () => {
+  const datamodel = `
     model User {
       id    Int @id
       posts Post[]
@@ -97,29 +96,29 @@ describe('schema generation', () => {
       name  String
     }
     `
-    const User = objectType({
-      name: 'User',
-      definition(t: any) {
-        t.model.id()
-        t.model.posts({ filtering: { id: true } })
-      },
-    })
-
-    const Post = objectType({
-      name: 'Post',
-      definition(t: any) {
-        t.model.id()
-        t.model.name()
-      },
-    })
-
-    const schema = await generateSchema(datamodel, [User, Post])
-
-    expect(printSchema(schema)).toMatchSnapshot()
+  const User = objectType({
+    name: 'User',
+    definition(t: any) {
+      t.model.id()
+      t.model.posts({ filtering: { id: true } })
+    },
   })
 
-  test('it exposes ordering only if ordering: true', async () => {
-    const datamodel = `
+  const Post = objectType({
+    name: 'Post',
+    definition(t: any) {
+      t.model.id()
+      t.model.name()
+    },
+  })
+
+  const schema = await generateSchema(datamodel, [User, Post])
+
+  expect(printSchema(schema)).toMatchSnapshot()
+})
+
+test('it exposes ordering only if ordering: true', async () => {
+  const datamodel = `
     model User {
       id    Int @id
       posts Post[]
@@ -130,29 +129,29 @@ describe('schema generation', () => {
       name  String
     }
     `
-    const User = objectType({
-      name: 'User',
-      definition(t: any) {
-        t.model.id()
-        t.model.posts({ ordering: true })
-      },
-    })
-
-    const Post = objectType({
-      name: 'Post',
-      definition(t: any) {
-        t.model.id()
-        t.model.name()
-      },
-    })
-
-    const schema = await generateSchema(datamodel, [User, Post])
-
-    expect(printSchema(schema)).toMatchSnapshot()
+  const User = objectType({
+    name: 'User',
+    definition(t: any) {
+      t.model.id()
+      t.model.posts({ ordering: true })
+    },
   })
 
-  test('it exposes id ordering', async () => {
-    const datamodel = `
+  const Post = objectType({
+    name: 'Post',
+    definition(t: any) {
+      t.model.id()
+      t.model.name()
+    },
+  })
+
+  const schema = await generateSchema(datamodel, [User, Post])
+
+  expect(printSchema(schema)).toMatchSnapshot()
+})
+
+test('it exposes id ordering', async () => {
+  const datamodel = `
     model User {
       id    Int @id
       posts Post[]
@@ -163,49 +162,48 @@ describe('schema generation', () => {
       name  String
     }
     `
-    const User = objectType({
-      name: 'User',
-      definition(t: any) {
-        t.model.id()
-        t.model.posts({ ordering: { id: true } })
-      },
-    })
-
-    const Post = objectType({
-      name: 'Post',
-      definition(t: any) {
-        t.model.id()
-        t.model.name()
-      },
-    })
-
-    const schema = await generateSchema(datamodel, [User, Post])
-
-    expect(printSchema(schema)).toMatchSnapshot()
+  const User = objectType({
+    name: 'User',
+    definition(t: any) {
+      t.model.id()
+      t.model.posts({ ordering: { id: true } })
+    },
   })
 
-  test('it exposes findOne and findMany', async () => {
-    const datamodel = `
+  const Post = objectType({
+    name: 'Post',
+    definition(t: any) {
+      t.model.id()
+      t.model.name()
+    },
+  })
+
+  const schema = await generateSchema(datamodel, [User, Post])
+
+  expect(printSchema(schema)).toMatchSnapshot()
+})
+
+test('it exposes findOne and findMany', async () => {
+  const datamodel = `
     model User {
       id    Int @id
     }
     `
-    const Query = objectType({
-      name: 'Query',
-      definition(t: any) {
-        t.crud.user()
-        t.crud.users()
-      },
-    })
-    const User = objectType({
-      name: 'User',
-      definition(t: any) {
-        t.model.id()
-      },
-    })
-
-    const schema = await generateSchema(datamodel, [User, Query])
-
-    expect(printSchema(schema)).toMatchSnapshot()
+  const Query = objectType({
+    name: 'Query',
+    definition(t: any) {
+      t.crud.user()
+      t.crud.users()
+    },
   })
+  const User = objectType({
+    name: 'User',
+    definition(t: any) {
+      t.model.id()
+    },
+  })
+
+  const schema = await generateSchema(datamodel, [User, Query])
+
+  expect(printSchema(schema)).toMatchSnapshot()
 })
