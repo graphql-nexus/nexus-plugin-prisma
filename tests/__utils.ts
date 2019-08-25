8c10dfc1f127e95978ac1e11966d5ac1acf1717c
Jason Kuhrt <jasonkuhrt@me.com>
2019-08-24 23:10:12 -0400

    use dynamicOutputProperty

--- tests/__utils.ts
+++ tests/__utils.ts
@@ -1,27 +1,27 @@
-import { makeSchema } from 'nexus';
-import { generateClient as generatePhoton } from '@prisma/photon';
-import tmp from 'tmp';
-import { getNexusPrismaRuntime } from '../src';
+import { makeSchema } from 'nexus'
+import { generateClient as generatePhoton } from '@prisma/photon'
+import tmp from 'tmp'
+import { getNexusPrismaRuntime } from '../src'
 
 export async function generateSchema(datamodel: string, types: any[]) {
-  const photonOutput = tmp.dirSync();
+  const photonOutput = tmp.dirSync()
 
   await generatePhoton({
     datamodel,
     outputDir: photonOutput.name,
     cwd: __dirname,
     transpile: true,
-  });
+  })
 
-  const { nexusPrismaRuntime } = getNexusPrismaRuntime(photonOutput.name);
-  const { nexusPrismaPlugin } = eval(nexusPrismaRuntime);
+  const { nexusPrismaRuntime } = getNexusPrismaRuntime(photonOutput.name)
+  const { nexusPrismaPlugin } = eval(nexusPrismaRuntime)
 
-  const nexusPrisma = nexusPrismaPlugin({ photon: (ctx: any) => ctx.photon });
+  const nexusPrisma = nexusPrismaPlugin({ photon: (ctx: any) => ctx.photon })
 
   const schema = makeSchema({
     types: [types, nexusPrisma],
     outputs: false,
-  });
+  })
 
-  return schema;
+  return schema
 }
