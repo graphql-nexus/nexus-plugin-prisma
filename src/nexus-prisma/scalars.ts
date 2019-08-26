41f7bd5a43ce506c9de722ca7aa55ae265fa331d
IngoValente <ingo@ingovalente.com>
2019-08-26 15:21:59 +0200

    Expose prism DateTime scalar as nexus method
    Make it possible to use the scalar in nexus definitions

--- src/nexus-prisma/scalars.ts
+++ src/nexus-prisma/scalars.ts
@@ -4,6 +4,7 @@ export const GQL_SCALARS_NAMES = ['Int', 'Float', 'String', 'ID', 'Boolean'];
 
 export const dateTimeScalar = scalarType({
   name: 'DateTime',
+  asNexusMethod: 'date',
   description: 'DateTime',
   parseLiteral(value) {
     return value;
