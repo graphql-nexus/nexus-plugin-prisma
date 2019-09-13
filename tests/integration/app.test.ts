e806e6e624a221d3048deb464a3f033311c5a8be
Jason Kuhrt <jasonkuhrt@me.com>
2019-09-13 09:27:11 -0400

    frontload most interesting typegen in snapshot

--- tests/integration/app.test.ts
+++ tests/integration/app.test.ts
@@ -1,6 +1,3 @@
-// TODO
-// Test ideas:
-// - show we gracefully handle case of photon import failing
 import * as cp from 'child_process'
 import * as path from 'path'
 import * as nexusBuilder from 'nexus/dist/builder'
@@ -17,6 +14,9 @@ afterAll(async () => {
   // test app in VSCode with actual IDE TS type check feedback.
   await fs.remove(relative('generated'))
 })
+
+// IDEA Future tests?
+// - show we gracefully handle case of photon import failing
 
 it('integrates together', async () => {
   // Run Prisma generation:
@@ -53,14 +53,19 @@ it('integrates together', async () => {
   // Assert the app type checks. In effect this is testing that our
   // typegen works. Snapshot our generated files for manual correctness
   // tracking.
+  //
   expect(relative('tsconfig.json')).toTypeCheck()
   expect(await getGenerated('schema.graphql')).toMatchSnapshot()
-  expect(await getGenerated('nexus-types/core.d.ts')).toMatchSnapshot()
   expect(await getGenerated('nexus-types/prisma.d.ts')).toMatchSnapshot()
+  expect(await getGenerated('nexus-types/core.d.ts')).toMatchSnapshot()
 })
 
 async function getGenerated(relPath: string): Promise<string> {
   return fs.readFile(path.join(generatedPath(relPath))).then(b => b.toString())
+}
+
+async function getRelative(relPath: string): Promise<string> {
+  return fs.readFile(path.join(relative(relPath))).then(b => b.toString())
 }
 
 function generatedPath(relPath: string): string {
