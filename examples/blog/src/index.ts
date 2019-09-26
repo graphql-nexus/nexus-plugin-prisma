f0f8b9e609e2d7493b36169b1f762cca0e676a83
Jason Kuhrt <jasonkuhrt@me.com>
2019-09-26 13:55:59 -0400

    docs: add nexus generate to example

--- examples/blog/src/index.ts
+++ examples/blog/src/index.ts
@@ -1,33 +1,9 @@
-import * as Path from 'path'
-import * as Yoga from 'graphql-yoga'
-import * as Nexus from 'nexus'
-import * as NexusPrisma from 'nexus-prisma'
-import * as types from './types'
+import { GraphQLServer } from 'graphql-yoga'
 import { Photon } from '@generated/photon'
+import schema from './schema'
 
 const photon = new Photon()
-const nexusPrisma = NexusPrisma.nexusPrismaPlugin({ types })
-
-const schema = Nexus.makeSchema({
-  types: [types, nexusPrisma],
-  outputs: {
-    typegen: Path.join(
-      __dirname,
-      '../node_modules/@types/__nexus-typegen__nexus-core/index.d.ts',
-    ),
-    schema: Path.join(__dirname, '../schema.graphql'),
-  },
-  typegenAutoConfig: {
-    sources: [
-      {
-        source: '@generated/photon',
-        alias: 'photon',
-      },
-    ],
-  },
-})
-
-const server = new Yoga.GraphQLServer({
+const server = new GraphQLServer({
   schema,
   context: () => ({ photon }),
 })
