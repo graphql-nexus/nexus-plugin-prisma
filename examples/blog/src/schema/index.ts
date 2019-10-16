9be936f9c79048413c414665936f91c22736877f
Jason Kuhrt <jasonkuhrt@me.com>
2019-10-17 00:28:54 +0200

    tidy example

--- examples/blog/src/schema/index.ts
+++ examples/blog/src/schema/index.ts
@@ -1,16 +1,14 @@
 import * as Nexus from 'nexus'
-import * as NexusPrisma from 'nexus-prisma'
+import { nexusPrismaPlugin } from 'nexus-prisma'
 import * as Query from './Query'
 import * as Mutation from './Mutation'
 import * as Blog from './Blog'
 import * as Post from './Post'
 import * as User from './User'
 
-const types = [Query, Mutation, Blog, Post, User]
-
 export default Nexus.makeSchema({
-  types,
-  plugins: [NexusPrisma.nexusPrismaPlugin()],
+  types: [Query, Mutation, Blog, Post, User],
+  plugins: [nexusPrismaPlugin()],
   typegenAutoConfig: {
     contextType: 'Context.Context',
     sources: [
