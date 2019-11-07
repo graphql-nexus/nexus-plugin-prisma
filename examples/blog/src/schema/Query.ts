ee4649c5e8927ebe4ccf86440b7d84a37c8d1b2c
Jason Kuhrt <jasonkuhrt@me.com>
2019-11-07 14:41:55 -0500

    docs: update strange code with issue ref

--- examples/blog/src/schema/Query.ts
+++ examples/blog/src/schema/Query.ts
@@ -18,13 +18,16 @@ export const Query = queryType({
         id: intArg({ required: true }),
       },
       resolve(_root, args, ctx) {
-        return ctx.photon.blogs
-          .findOne({
-            where: {
-              id: args.id,
-            },
-          })
-          .then()
+        return (
+          ctx.photon.blogs
+            .findOne({
+              where: {
+                id: args.id,
+              },
+            })
+            // https://github.com/prisma/photonjs/issues/288
+            .then()
+        )
       },
     })
 
