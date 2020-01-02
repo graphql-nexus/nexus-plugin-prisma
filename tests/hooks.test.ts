82be4e7860f4bb54015d1a2ed5f6974e9fbffc53
Flavian DESVERNE <flavian.desverne@epitech.eu>
2020-01-02 13:37:08 +0100

    add tests for new hook

--- tests/hooks.test.ts
+++ tests/hooks.test.ts
@@ -6,7 +6,7 @@ it('in dev stage, warns when wrong projected field or crud', async () => {
 
   const datamodel = `
   model User {
-    id  Int @id
+    id  Int @id @default(autoincrement())
     username String
   }
 `
@@ -44,7 +44,7 @@ it('in undefined stages, warns when wrong projected field or crud', async () => 
 
   const datamodel = `
   model User {
-    id  Int @id
+    id  Int @id @default(autoincrement())
     username String
   }
 `
@@ -82,7 +82,7 @@ it('in other stages, throws error when wrong projected field or crud', async () 
 
   const datamodel = `
   model User {
-    id  Int @id
+    id  Int @id @default(autoincrement())
     username String
   }
 `
@@ -116,7 +116,7 @@ it('in production, throws if a wrong arg named is passed to filtering', async ()
 
   const datamodel = `
   model User {
-    id  Int @id
+    id  Int @id @default(autoincrement())
   }
 `
 
@@ -148,7 +148,7 @@ it('in dev stage, warns if a wrong arg named is passed to filtering', async () =
 
   const datamodel = `
   model User {
-    id  Int @id
+    id  Int @id @default(autoincrement())
     name String
   }
 `
@@ -183,7 +183,7 @@ it('in production, throws if a wrong arg named is passed to ordering', async () 
 
   const datamodel = `
   model User {
-    id  Int @id
+    id  Int @id @default(autoincrement())
   }
 `
 
@@ -215,7 +215,7 @@ it('in dev stage, warns if a wrong arg named is passed to ordering', async () =>
 
   const datamodel = `
   model User {
-    id  Int @id
+    id  Int @id @default(autoincrement())
     name String
   }
 `
@@ -243,4 +243,78 @@ it('in dev stage, warns if a wrong arg named is passed to ordering', async () =>
     Warning: Your GraphQL \`Query\` object definition is projecting a relational field \`users\`. On it, you are declaring that clients be able to order by Prisma \`User\` model field \`unknownField\`. However, your Prisma model \`User\` model has no such field \`unknownField\`
     "
   `)
+})
+
+it('in dev stage, warns if a graphql typename does not map to a prisma name but t.model.field() was used', async () => {
+  process.env.NODE_ENV = 'development'
+
+  const datamodel = `
+  model User {
+    id   Int @id @default(autoincrement())
+    name String
+    age  Int
+  }
+`
+
+  const User = objectType({
+    name: 'GraphQLTypeNameNotMappingToAPrismaModelName',
+    definition(t: any) {
+      t.model.id()
+      t.model.name()
+      t.model.age()
+    },
+  })
+
+  const { schema, $output } = await mockConsoleLog(() =>
+    generateSchemaAndTypes(datamodel, [User]),
+  )
+
+  expect(schema).toMatchInlineSnapshot(`
+    "type GraphQLTypeNameNotMappingToAPrismaModelName
+
+    type Query {
+      ok: Boolean!
+    }
+    "
+  `)
+  expect($output).toMatchInlineSnapshot(`
+    "
+    Warning: Your GraphQL \`GraphQLTypeNameNotMappingToAPrismaModelName\` object definition is attempting to expose some Prisma model fields named \`\\"id\\", \\"name\\", \\"age\\"\`, but there is no such Prisma model called \`GraphQLTypeNameNotMappingToAPrismaModelName\`
+    Warning: If this is not intentional, make sure you don't have a typo in your GraphQL type name \`GraphQLTypeNameNotMappingToAPrismaModelName\`
+    Warning: If this is intentional, pass the mapped Prisma model name as parameter like so \`t.model('<PrismaModelName>').<FieldName>()\`
+    "
+  `)
+})
+
+it('in prod stage, throw error if a graphql typename does not map to a prisma name but t.model.field() was used', async () => {
+  process.env.NODE_ENV = 'production'
+
+  const datamodel = `
+  model User {
+    id   Int @id @default(autoincrement())
+    name String
+    age  Int
+  }
+`
+
+  const User = objectType({
+    name: 'GraphQLTypeNameNotMappingToAPrismaModelName',
+    definition(t: any) {
+      t.model.id()
+      t.model.name()
+      t.model.age()
+    },
+  })
+
+  try {
+    const { schema } = await generateSchemaAndTypes(datamodel, [User])
+
+    expect(schema).toMatchInlineSnapshot()
+  } catch (e) {
+    expect(e.message).toMatchInlineSnapshot(`
+      "Your GraphQL \`GraphQLTypeNameNotMappingToAPrismaModelName\` object definition is attempting to expose some Prisma model fields named \`\\"id\\", \\"name\\", \\"age\\"\`, but there is no such Prisma model called \`GraphQLTypeNameNotMappingToAPrismaModelName\`
+      If this is not intentional, make sure you don't have a typo in your GraphQL type name \`GraphQLTypeNameNotMappingToAPrismaModelName\`
+      If this is intentional, pass the mapped Prisma model name as parameter like so \`t.model('<PrismaModelName>').<FieldName>()\`"
+    `)
+  }
 })
