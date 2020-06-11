dc5b852328386f710e3769934f978ce1579dcc47
Jason Kuhrt <jasonkuhrt@me.com>
2020-06-11 11:54:20 -0400

    fix tests

--- tests/__utils.ts
+++ tests/__utils.ts
@@ -6,9 +6,9 @@ import stripAnsi from 'strip-ansi'
 import * as NexusPrismaBuilder from '../src/builder'
 import { DmmfDocument } from '../src/dmmf'
 import { transform, TransformOptions } from '../src/dmmf/transformer'
+import { relayLikePaginationStrategy } from '../src/pagination'
 import { render as renderTypegen } from '../src/typegen'
 import { getEnginePath } from './__ensure-engine'
-import { relayLikePaginationStrategy } from '../src/pagination'
 
 export const createNexusPrismaInternal = (
   options: Omit<NexusPrismaBuilder.InternalOptions, 'nexusBuilder'>,
@@ -70,7 +70,7 @@ export async function generateSchemaAndTypes(
     schema,
     typegen: renderTypegen({
       dmmf,
-      prismaClientPath: '@prisma/client',
+      prismaClientImportId: '@prisma/client',
       paginationStrategy: relayLikePaginationStrategy,
     }),
   }
@@ -98,7 +98,7 @@ export async function generateSchemaAndTypesWithoutThrowing(
   })
   const typegen = renderTypegen({
     dmmf,
-    prismaClientPath: '@prisma/client',
+    prismaClientImportId: '@prisma/client',
     paginationStrategy: relayLikePaginationStrategy,
   })
 
