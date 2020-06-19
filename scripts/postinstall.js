941a456509cc98a058e0a341789d9c4e54543879
Flavian DESVERNE <flavian.desverne@epitech.eu>
2020-06-19 18:56:09 +0200

    fix postinstall

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -3,12 +3,19 @@ const jetpack = require('fs-jetpack')
 const path = require('path')
 // process.env.PWD is undefined on Windows https://github.com/mrblueblue/gettext-loader/issues/18
 
-const pwd = process.cwd()
-const from = path.join(__dirname, '..', 'global-type.d.ts')
-const destDir = path.join(pwd, '..', '@types', 'nexus-plugin-prisma')
+if (process.env.INIT_CWD) {
+  process.chdir(process.env.INIT_CWD)
+}
 
-jetpack.dir(destDir)
-jetpack.copy(from, path.join(destDir, 'index.d.ts'), { overwrite: true })
+const pwd = process.cwd()
+const from = path.join(pwd, 'node_modules', 'nexus-plugin-prisma', 'global-type.d.ts')
+
+if (jetpack.exists(from)) {
+  const destDir = path.join(pwd, '..', '@types', 'nexus-plugin-prisma')
+
+  jetpack.dir(destDir)
+  jetpack.copy(from, path.join(destDir, 'index.d.ts'), { overwrite: true })
+}
 
 console.log(chalk.bold.yellowBright('----------------------------------'))
 console.log(
