b067bc599cc00882f3295fe58a86844ef01db800
Flavian DESVERNE <flavian.desverne@epitech.eu>
2020-06-19 19:09:39 +0200

    add postinstall comment

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -11,6 +11,7 @@ const pwd = process.cwd()
 const nodeModulesFolder = path.join(pwd, 'node_modules')
 const from = path.resolve(nodeModulesFolder, 'nexus-plugin-prisma', 'global-type.d.ts')
 
+// Prevent from crashing if the typings can't be found for whatever reason
 if (jetpack.exists(from)) {
   const destDir = path.resolve(nodeModulesFolder, '@types', 'nexus-plugin-prisma')
 
