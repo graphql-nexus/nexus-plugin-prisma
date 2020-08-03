59ed34e3c6bf917e2997323dca6a555acd6e994d
Flavian DESVERNE <flavian.desverne@epitech.eu>
2020-08-03 15:19:10 +0200

    add script to ensure all prisma deps are synced

---
+++ scripts/ensure-deps-sync.js
@@ -0,0 +1,16 @@
+const packageJson = require('../package.json')
+
+const prismaDeps = [
+  ...Object.entries(packageJson.dependencies),
+  ...Object.entries(packageJson.devDependencies),
+].filter(([depName]) => depName.startsWith('@prisma/'))
+const versionReference = prismaDeps[0][1]
+
+const invalidDeps = prismaDeps.filter(([, version]) => version !== versionReference)
+
+if (invalidDeps.length > 0) {
+  console.log('Some of your prisma dependencies are not in sync. ', invalid.join(', '))
+  process.exit(1);
+} else {
+  console.log('All prisma deps are in sync')
+}
