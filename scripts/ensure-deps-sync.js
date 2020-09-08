9da61032afa55532131495ebfc9643122e30337f
Flavian Desverne <flavian.desverne@epitech.eu>
2020-09-08 16:35:19 +0200

    fix(deps): upgrade prisma to 2.6.2 (#829)

--- scripts/ensure-deps-sync.js
+++ scripts/ensure-deps-sync.js
@@ -9,7 +9,7 @@ const versionReference = prismaDeps[0][1]
 const invalidDeps = prismaDeps.filter(([, version]) => version !== versionReference)
 
 if (invalidDeps.length > 0) {
-  console.log('Some of your prisma dependencies are not in sync. ', invalid.join(', '))
+  console.log('Some of your prisma dependencies are not in sync. ', invalidDeps.join(', '))
   process.exit(1);
 } else {
   console.log('All prisma deps are in sync')
