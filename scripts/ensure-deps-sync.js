beb429a007ff34f9aa07011a64af454cfb128174
Flavian DESVERNE <flavian.desverne@epitech.eu>
2020-10-07 17:42:54 +0200

    fix script

--- scripts/ensure-deps-sync.js
+++ scripts/ensure-deps-sync.js
@@ -5,13 +5,21 @@ const prismaDeps = [
   ...Object.entries(packageJson.devDependencies),
   ...Object.entries(packageJson.peerDependencies),
 ].filter(([depName]) => depName.startsWith('@prisma/'))
-const versionReference = prismaDeps[0][1]
+const versionReference = stripCaret(prismaDeps[0][1])
 
-const invalidDeps = prismaDeps.filter(([, version]) => version !== versionReference)
+const invalidDeps = prismaDeps.filter(([, version]) => stripCaret(version) !== versionReference)
 
 if (invalidDeps.length > 0) {
   console.log('Some of your prisma dependencies are not in sync. ', invalidDeps.join(', '))
   process.exit(1)
 } else {
   console.log('All prisma deps are in sync')
+}
+
+function stripCaret(version) {
+  if (version.startsWith('^')) {
+    return version.substring(1)
+  }
+
+  return version
 }
