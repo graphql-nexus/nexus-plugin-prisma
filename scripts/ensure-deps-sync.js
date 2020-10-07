e9b976785be4c065aa619fb70ef1628dd8ac7994
Flavian DESVERNE <flavian.desverne@epitech.eu>
2020-10-07 18:18:05 +0200

    make prisma peer range stricter

--- scripts/ensure-deps-sync.js
+++ scripts/ensure-deps-sync.js
@@ -1,13 +1,14 @@
+const semver = require('semver')
 const packageJson = require('../package.json')
 
 const prismaDeps = [
   ...Object.entries(packageJson.dependencies),
   ...Object.entries(packageJson.devDependencies),
-  ...Object.entries(packageJson.peerDependencies),
 ].filter(([depName]) => depName.startsWith('@prisma/'))
-const versionReference = stripCaret(prismaDeps[0][1])
 
-const invalidDeps = prismaDeps.filter(([, version]) => stripCaret(version) !== versionReference)
+const versionRange = packageJson.peerDependencies['@prisma/client']
+
+const invalidDeps = prismaDeps.filter(([, version]) => semver.satisfies(version, versionRange) === false)
 
 if (invalidDeps.length > 0) {
   console.log('Some of your prisma dependencies are not in sync. ', invalidDeps.join(', '))
