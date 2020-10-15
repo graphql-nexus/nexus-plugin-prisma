472211eed774e18eec249301aa7b7663e0b6786f
Jason Kuhrt <jasonkuhrt@me.com>
2020-10-15 12:53:37 -0400

    add truths subscription for kicks

--- examples/hello-world/api.ts
+++ examples/hello-world/api.ts
@@ -78,6 +78,19 @@ const apollo = new ApolloServer({
       }),
       subscriptionType({
         definition(t) {
+          t.boolean('truths', {
+            subscribe() {
+              return (async function* () {
+                while (true) {
+                  await new Promise((res) => setTimeout(res, 1000))
+                  yield Math.random() > 0.5
+                }
+              })() as any
+            },
+            resolve(eventData) {
+              return eventData
+            },
+          })
           t.field('createOneUserEvents', {
             type: 'User',
             subscribe() {
