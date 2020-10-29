ba0868720b9fb719598d78978ad5d110286978d0
Luc Vauvillier <luc.vauvillier@gmail.com>
2020-10-29 10:30:25 +0100

    Add test

---
+++ tests/schema/atomicOperations.test.ts
@@ -0,0 +1,69 @@
+import { objectType } from '@nexus/schema'
+import { paginationStrategies } from '../../src/pagination'
+import { generateSchemaAndTypes } from '../__utils'
+
+it('support atomic operations (default)', async () => {
+  const datamodel = `
+  model ModelName {
+    id      Int @id @default(autoincrement())
+    name    String
+    amount  Int
+  }
+`
+  const ModelName = objectType({
+    name: 'ModelName',
+    definition(t: any) {
+      t.model.id()
+    },
+  })
+
+  const Mutation = objectType({
+    name: 'Mutation',
+    definition(t: any) {
+      t.crud.createOneModelName()
+      t.crud.deleteOneModelName()
+      t.crud.updateOneModelName()
+      t.crud.upsertOneModelName()
+      t.crud.updateManyModelName()
+    },
+  })
+
+  const { schemaString: schema, typegen } = await generateSchemaAndTypes(datamodel, [Mutation, ModelName])
+
+  expect(schema).toMatchSnapshot('schema')
+  expect(typegen).toMatchSnapshot('typegen')
+})
+
+it('disable atomic operations', async () => {
+  const datamodel = `
+  model ModelName {
+    id      Int @id @default(autoincrement())
+    name    String
+    amount  Int
+  }
+`
+  const ModelName = objectType({
+    name: 'ModelName',
+    definition(t: any) {
+      t.model.id()
+    },
+  })
+
+  const Mutation = objectType({
+    name: 'Mutation',
+    definition(t: any) {
+      t.crud.createOneModelName()
+      t.crud.deleteOneModelName()
+      t.crud.updateOneModelName()
+      t.crud.upsertOneModelName()
+      t.crud.updateManyModelName()
+    },
+  })
+
+  const { schemaString: schema, typegen } = await generateSchemaAndTypes(datamodel, [Mutation, ModelName], {
+    atomicOperations: false,
+  })
+
+  expect(schema).toMatchSnapshot('schema')
+  expect(typegen).toMatchSnapshot('typegen')
+})
