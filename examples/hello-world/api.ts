b32505b19e79c0f4977cc19b7d0e890aaf9881b2
Jason Kuhrt <jasonkuhrt@me.com>
2020-11-12 09:50:14 -0500

    chore: tidy hello world example

--- examples/hello-world/api.ts
+++ examples/hello-world/api.ts
@@ -1,113 +1,119 @@
 import {
+  ApolloServer,
+  PubSub
+} from 'apollo-server-express'
+import express from 'express'
+import {
+  DateTimeResolver,
+  JSONObjectResolver
+} from 'graphql-scalars'
+import * as HTTP from 'http'
+import { nexusPrisma } from 'nexus-plugin-prisma'
+import * as path from 'path'
+import {
   asNexusMethod,
   makeSchema,
   mutationType,
   objectType,
   queryType,
-  subscriptionType,
+  subscriptionType
 } from '@nexus/schema'
 import { PrismaClient } from '@prisma/client'
-import { ApolloServer, PubSub } from 'apollo-server-express'
-import express from 'express'
-import { DateTimeResolver, JSONObjectResolver } from 'graphql-scalars'
-import * as HTTP from 'http'
-import { nexusPrisma } from 'nexus-plugin-prisma'
-import * as path from 'path'
 
 const pubsub = new PubSub()
-
 const prisma = new PrismaClient()
+
+const schema = makeSchema({
+  shouldExitAfterGenerateArtifacts: process.env.NEXUS_SHOULD_EXIT_AFTER_GENERATE_ARTIFACTS === 'true',
+  typegenAutoConfig: {
+    contextType: '{ prisma: PrismaClient.PrismaClient }',
+    sources: [{ source: '.prisma/client', alias: 'PrismaClient' }],
+  },
+  outputs: {
+    typegen: path.join(__dirname, 'node_modules/@types/nexus-typegen/index.d.ts'),
+    schema: path.join(__dirname, './api.graphql'),
+  },
+  plugins: [
+    nexusPrisma({
+      experimentalCRUD: true,
+    }),
+  ],
+  types: [
+    asNexusMethod(JSONObjectResolver, 'json'),
+    asNexusMethod(DateTimeResolver, 'date'),
+    queryType({
+      definition(t) {
+        t.crud.user()
+        t.crud.users({ ordering: true })
+        t.crud.post()
+        t.crud.posts({ filtering: true })
+      },
+    }),
+    mutationType({
+      definition(t) {
+        t.crud.createOneUser({
+          async resolve(root, args, ctx, info, originalResolve) {
+            const data = originalResolve(root, args, ctx, info)
+            await pubsub.publish('user_added', { data })
+            return data
+          },
+        })
+        t.crud.createOnePost()
+        t.crud.deleteOneUser()
+        t.crud.deleteOnePost()
+      },
+    }),
+    objectType({
+      name: 'User',
+      definition(t) {
+        t.model.id()
+        t.model.metadata()
+        t.model.email()
+        t.model.birthDate()
+        t.model.posts()
+      },
+    }),
+    objectType({
+      name: 'Post',
+      definition(t) {
+        t.model.id()
+        t.model.authors()
+      },
+    }),
+    subscriptionType({
+      definition(t) {
+        t.boolean('truths', {
+          subscribe() {
+            return (async function* () {
+              while (true) {
+                await new Promise((res) => setTimeout(res, 1000))
+                yield Math.random() > 0.5
+              }
+            })() as any
+          },
+          resolve(eventData) {
+            return eventData
+          },
+        })
+        t.field('createOneUserEvents', {
+          type: 'User',
+          subscribe() {
+            return pubsub.asyncIterator('user_added')
+          },
+          async resolve(userPromise) {
+            const user = await userPromise.data
+            return user
+          },
+        })
+      },
+    }),
+  ],
+})
 
 const apollo = new ApolloServer({
   context: () => ({ prisma }),
-  schema: makeSchema({
-    shouldExitAfterGenerateArtifacts: process.env.NEXUS_SHOULD_EXIT_AFTER_GENERATE_ARTIFACTS === 'true',
-    typegenAutoConfig: {
-      contextType: '{ prisma: PrismaClient.PrismaClient }',
-      sources: [{ source: '.prisma/client', alias: 'PrismaClient' }],
-    },
-    outputs: {
-      typegen: path.join(__dirname, 'node_modules/@types/nexus-typegen/index.d.ts'),
-      schema: path.join(__dirname, './api.graphql'),
-    },
-    plugins: [
-      nexusPrisma({
-        experimentalCRUD: true,
-      }),
-    ],
-    types: [
-      asNexusMethod(JSONObjectResolver, 'json'),
-      asNexusMethod(DateTimeResolver, 'date'),
-      queryType({
-        definition(t) {
-          t.crud.user()
-          t.crud.users({ ordering: true })
-          t.crud.post()
-          t.crud.posts({ filtering: true })
-        },
-      }),
-      mutationType({
-        definition(t) {
-          t.crud.createOneUser({
-            async resolve(root, args, ctx, info, originalResolve) {
-              const data = originalResolve(root, args, ctx, info)
-              await pubsub.publish('user_added', { data })
-              return data
-            },
-          })
-          t.crud.createOnePost()
-          t.crud.deleteOneUser()
-          t.crud.deleteOnePost()
-        },
-      }),
-      objectType({
-        name: 'User',
-        definition(t) {
-          t.model.id()
-          t.model.metadata()
-          t.model.email()
-          t.model.birthDate()
-          t.model.posts()
-        },
-      }),
-      objectType({
-        name: 'Post',
-        definition(t) {
-          t.model.id()
-          t.model.authors()
-        },
-      }),
-      subscriptionType({
-        definition(t) {
-          t.boolean('truths', {
-            subscribe() {
-              return (async function* () {
-                while (true) {
-                  await new Promise((res) => setTimeout(res, 1000))
-                  yield Math.random() > 0.5
-                }
-              })() as any
-            },
-            resolve(eventData) {
-              return eventData
-            },
-          })
-          t.field('createOneUserEvents', {
-            type: 'User',
-            subscribe() {
-              return pubsub.asyncIterator('user_added')
-            },
-            async resolve(userPromise) {
-              const user = await userPromise.data
-              return user
-            },
-          })
-        },
-      }),
-    ],
-  }),
+  schema,
 })
-
 const app = express()
 const http = HTTP.createServer(app)
 
