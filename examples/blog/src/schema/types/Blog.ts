c5aa17966beb3a1bb32105dbbc92bf80494c4639
Jason Kuhrt <jasonkuhrt@me.com>
2020-11-18 16:42:41 -0500

    chor(docs): use queryField

--- examples/blog/src/schema/types/Blog.ts
+++ examples/blog/src/schema/types/Blog.ts
@@ -1,4 +1,4 @@
-import { extendType, intArg, objectType, stringArg } from '@nexus/schema'
+import { intArg, objectType, queryField, stringArg } from '@nexus/schema'
 
 export const Blog = objectType({
   name: 'Blog',
@@ -18,63 +18,52 @@ export const Blog = objectType({
   },
 })
 
-export const Mutation = extendType({
-  type: 'Mutation',
-  definition(t) {
-    t.crud.createOneBlog()
-    t.crud.updateManyBlog()
-  },
-})
+export const Query = queryField((t) => {
+  t.crud.blogs({
+    pagination: false,
+  })
+  t.crud.users({ filtering: true, alias: 'people' })
+  t.crud.posts({ type: 'CustomPost', ordering: true, filtering: true })
 
-export const Query = extendType({
-  type: 'Query',
-  definition(t) {
-    t.crud.blogs({
-      pagination: false,
-    })
-    t.crud.users({ filtering: true, alias: 'people' })
-    t.crud.posts({ type: 'CustomPost', ordering: true, filtering: true })
+  //
+  // Examples showing custom resolvers
+  //
 
-    //
-    // Examples showing custom resolvers
-    //
-
-    t.field('blog', {
-      type: 'Blog',
-      args: {
-        id: intArg({ required: true }),
-      },
-      resolve(_root, args, ctx) {
-        return ctx.prisma.blog
-          .findOne({
-            where: {
-              id: args.id,
-            },
-          })
-          .then((result) => {
-            if (result === null) {
-              throw new Error(`No blog with id of "${args.id}"`)
-            }
-            return result
-          })
-      },
-    })
-
-    t.field('blogsLike', {
-      type: 'Blog',
-      list: true,
-      args: {
-        name: stringArg(),
-        viewCount: intArg(),
-      },
-      resolve(_root, args, ctx) {
-        return ctx.prisma.blog.findMany({
+  t.field('blog', {
+    type: 'Blog',
+    args: {
+      id: intArg({ required: true }),
+    },
+    resolve(_root, args, ctx) {
+      return ctx.prisma.blog
+        .findOne({
           where: {
-            name: args.name ?? undefined,
-            viewCount: args.viewCount ?? undefined,
+            id: args.id,
           },
         })
-      },
-    })
-  },
+        .then((result) => {
+          if (result === null) {
+            throw new Error(`No blog with id of "${args.id}"`)
+          }
+          return result
+        })
+    },
+  })
+
+  t.field('blogsLike', {
+    type: 'Blog',
+    list: true,
+    args: {
+      name: stringArg(),
+      viewCount: intArg(),
+    },
+    resolve(_root, args, ctx) {
+      return ctx.prisma.blog.findMany({
+        where: {
+          name: args.name ?? undefined,
+          viewCount: args.viewCount ?? undefined,
+        },
+      })
+    },
+  })
 })
