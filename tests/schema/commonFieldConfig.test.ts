44938c1c67129256a7dd4087715e2c0369d12cfc
Mason Bourgeois <mason.bourgeois@gmail.com>
2020-12-15 12:07:00 -0600

    add tests for forwarding CommonFieldConfig

---
+++ tests/schema/commonFieldConfig.test.ts
@@ -0,0 +1,56 @@
+import { objectType } from '@nexus/schema'
+import { generateSchemaAndTypes } from '../__utils'
+
+it('forwards description & deprecation on all field types', async () => {
+  const datamodel = `
+    model User {
+      id  Int @id @default(autoincrement())
+
+      name String
+      emailConfirmed Boolean
+      birthYear Int
+      heightM Float
+
+      favouriteColor  Color
+
+      latestPost Post? @relation("latestPostId")
+      posts Post[] @relation("userId")
+    }
+
+    enum Color {
+      Red
+      Green
+      Blue
+    }
+
+    model Post {
+      id  Int @id @default(autoincrement())
+      title String
+      body String
+      user User @relation("userId")
+    }
+  `
+
+  const Post = objectType({
+    name: 'Post',
+    definition(t) {
+      t.model.id()
+    },
+  })
+
+  const User = objectType({
+    name: 'User',
+    definition(t: any) {
+      t.model.id({ description: 'The unique id', deprecation: 'never' })
+      t.model.name({ description: "The user's full name", deprecation: 'who needs names' })
+      t.model.heightM({ description: "The user's height, in meters", deprecation: "size doesn't matter" })
+      t.model.favouriteColor({ description: "The user's favorite color", deprecation: 'no longer required' })
+      t.model.posts({ description: "All the user's blog posts", deprecation: 'no more blog' })
+      t.model.latestPost({ description: "The user's last blog post", deprecation: 'not here anymore' })
+    },
+  })
+
+  const { schemaString: schema } = await generateSchemaAndTypes(datamodel, [User, Post])
+
+  expect(schema).toMatchSnapshot('commonFieldsOutput')
+})
