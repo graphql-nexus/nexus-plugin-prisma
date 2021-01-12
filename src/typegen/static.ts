eef9baf36bfe0418dcc5a3275c4b5d43db507f35
Pascal Sthamer <sthamer.pascal@gmail.com>
2021-01-12 01:56:57 +0100

    fix: fix root type in CustomFieldResolver
    Closes #1004 
    Closes #1011

--- src/typegen/static.ts
+++ src/typegen/static.ts
@@ -26,7 +26,7 @@ declare global {
 export type ModelNameInGraphQLTypes<ReturnType> = ReturnType extends core.GetGen<'objectNames'> ? true : false
 
 export type CustomFieldResolver<TypeName extends string, FieldName extends string> = (
-  root: core.RootValueField<TypeName, FieldName>,
+  root: core.RootValue<TypeName>,
   args: core.ArgsValue<TypeName, FieldName>,
   context: core.GetGen<'context'>,
   info: GraphQLResolveInfo,
